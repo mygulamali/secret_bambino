8e37e582c31389bb2da9c8dc4adc9114dcc493dc
Murtaza Gulamali <mygulamali@gmail.com>
2014-12-12 17:39:55 +0000

    Create list of assigned Bambinos.

--- lib/secret_bambino/bambinos.rb
+++ lib/secret_bambino/bambinos.rb
@@ -4,11 +4,26 @@ module SecretBambino
       load_from_yaml(filename)
     end
 
+    def assigned_bambinos
+      @assigned_bambinos ||= shuffle(@bambinos)
+    end
+
     private
 
     def load_from_yaml(filename)
       data = YAML::load(File.open(filename))
       @bambinos = data['bambinos']
-    end    
+    end
+
+    # variant of Fisher-Yates shuffle (ie. not the same as array.shuffle)
+    def shuffle(ary)
+      ary_dup = ary.dup
+      n = ary_dup.length
+      (0...n).each do |i|
+        j = rand([i+1, n-1].min...n)
+        ary_dup[i], ary_dup[j] = ary_dup[j], ary_dup[i]
+      end
+      ary_dup
+    end
   end
 end
